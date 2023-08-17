8ad96a610297bcf9eaca319d3c23d590eaace72b
AkiraTerui <hanabi0111@gmail.com>
2023-08-17 10:03:30 -0400

    initial commit

---
+++ code/script_week1.R
@@ -0,0 +1,16 @@
+
+# sample script -----------------------------------------------------------
+
+# calculate mean
+a <- c(1, 4, 8)
+mu_a <- mean(a)
+
+print(mu_a)
+
+# another sample script ---------------------------------------------------
+
+# calculate mean
+b <- c(2, 1, 8)
+med_b <- median(b)
+
+print(med_b)
