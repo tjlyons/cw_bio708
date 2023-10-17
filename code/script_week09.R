5b29e01c03bef091d8374d4ea1ba65f47024801b
AkiraTerui <hanabi0111@gmail.com>
2023-10-17 10:28:24 -0400

    edit: week 9

--- code/script_week09.R
+++ code/script_week09.R
@@ -77,3 +77,90 @@ df_algae %>%
                    y = biomass,
                    yend = biomass - eps),
                linetype = "dashed")
+
+
+# lab ---------------------------------------------------------------------
+
+## 6.4.1 Develop regression models
+
+### apply filter function separately
+df1 <- iris %>% 
+  filter(Species == "setosa")
+
+m1 <- lm(Sepal.Width ~ Petal.Width,
+         data = df1)
+
+summary(m1)
+
+df2 <- iris %>% 
+  filter(Species == "versicolor")
+
+m2 <- lm(Sepal.Width ~ Petal.Width,
+         data = df2)
+
+summary(m2)
+
+df3 <- iris %>% 
+  filter(Species == "virginica")
+
+m3 <- lm(Sepal.Width ~ Petal.Width,
+         data = df3)
+
+summary(m3)
+
+### skip typing species names
+sp <- unique(iris$Species)
+
+df1 <- iris %>% 
+  filter(Species == sp[1])
+
+df2 <- iris %>% 
+  filter(Species == sp[2])
+
+df3 <- iris %>% 
+  filter(Species == sp[3])
+
+### lapply
+sp <- unique(iris$Species)
+
+list0 <- lapply(X = 1:3, function(i) {
+  # i will be substituted by what's in `X`...1, 2, 3
+  df_temp <- iris %>% 
+    filter(Species == sp[i])
+  
+  m <- lm(Sepal.Width ~ Petal.Width,
+          data = df_temp)
+  return(m)
+})
+
+list0 <- lapply(X = sp, function(i) {
+  # i will be substituted by what's in `X`...setota, versicolor, virginica
+  df_temp <- iris %>% 
+    filter(Species == i)
+  
+  m <- lm(Sepal.Width ~ Petal.Width,
+          data = df_temp)
+  return(m)
+})
+
+## 6.4.2 Multiple explanatory variables
+### add the second predictor to lm()
+### lm(y ~ x1 + x2, ...)
+
+x1 <- lm(Sepal.Width ~ Petal.Width + Petal.Length,
+         data = df1)
+
+summary(m1)
+summary(x1)
+
+x2 <- lm(Sepal.Width ~ Petal.Width + Petal.Length,
+         data = df2)
+
+summary(m2)
+summary(x2)
+
+x3 <- lm(Sepal.Width ~ Petal.Width + Petal.Length,
+         data = df3)
+
+summary(m3)
+summary(x3)
