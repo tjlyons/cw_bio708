6685a77c568851d7372d43a5776918060fd2babf
AkiraTerui <hanabi0111@gmail.com>
2023-10-19 10:33:27 -0400

    edit: linear model

---
+++ code/script_week10.R
@@ -0,0 +1,91 @@
+#' ---
+#' title: "Report week9"
+#' output: html_document
+#' date: "2023-10-12"
+#' author: Akira Terui
+#' ---
+
+library(tidyverse)
+
+# read data
+df_fl <- read_csv(here::here("data_raw/data_fish_length.csv"))
+
+m <- lm(length ~ lake,
+        data = df_fl)
+
+
+# t-test ------------------------------------------------------------------
+
+# calculate mean for lake a
+## base R version
+mu_a <- mean(df_fl$length[df_fl$lake == "a"])
+
+## dplyr version
+mu_a <- df_fl %>% 
+  filter(lake == "a") %>% # pick lake "a"
+  pull(length) %>% # pull out column "length", breaking a tibble structure
+  mean() # take mean
+
+# calculate mean for lake b
+mu_b <- df_fl %>% 
+  filter(lake == "b") %>% # pick lake "a"
+  pull(length) %>% # pull out column "length", breaking a tibble structure
+  mean() # take mean
+
+# diffrence between two groups
+b <- mu_b - mu_a
+
+# compare with t-test
+
+## lm summary
+summary(m)
+
+## apply t-test
+a <- df_fl %>% 
+  filter(lake == "a") %>% 
+  pull(length)
+
+b <- df_fl %>% 
+  filter(lake == "b") %>% 
+  pull(length)
+
+t.test(b, a, var.equal = TRUE)
+
+
+# anova -------------------------------------------------------------------
+
+df_anova <- read_csv(here::here("data_raw/data_fish_length_anova.csv"))
+
+m_anova <- lm(length ~ lake,
+              data = df_anova)
+
+# get difference from lake a
+df_mu <- df_anova %>% 
+  group_by(lake) %>% 
+  summarize(mu = mean(length)) %>% 
+  arrange(lake) %>% 
+  mutate(diff = mu - mu[1])
+
+v_mu <- pull(df_mu, mu)
+names(v_mu) <- pull(df_mu, lake)
+
+## do anova with aov()
+m_aov <- aov(length ~ lake,
+             data = df_anova)
+
+## compare lm() and aov() output
+summary(m_aov)
+summary(m_anova)
+
+# combine different types of predictors -----------------------------------
+
+iris <- as_tibble(iris)
+
+m_iris <- lm(Petal.Length ~ Petal.Width + Species,
+             data = iris)
+
+summary(m_iris)
+
+
+# lab ---------------------------------------------------------------------
+
