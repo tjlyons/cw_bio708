2455c8044890a5332fc9b717f037011d84f5a94e
tjlyons <tjlyons@uncg.edu>
2025-08-28 10:21:07 -0400

    tidyverse, reshape

--- code/tidyverse02.R
+++ code/tidyverse02.R
@@ -27,7 +27,22 @@ df_eps <- iris_sub %>%
 
 # Reshaping ---------------------------------------------------------------
 
+## wide format
+iris_w <- iris_sub %>% 
+  mutate(id = rep(1:3, 3)) %>%  # add an ID column
+  select(id, Sepal.Length, Species) %>% 
+  pivot_wider(id_cols = "id", # unique row ID based on
+              values_from = "Sepal.Length", # values in each cell from
+              names_from = "Species") # new column names from
 
+## long format
+iris_l <- iris_w %>% 
+  pivot_longer(cols = c("setosa", 
+                        "versicolor",
+                        "virginica"),
+               names_to = "Species",
+               values_to = "Sepal.Length"
+               )
 
 
 
