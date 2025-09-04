c1470750d91a8335267dd6d9903eeafbfe3cef7b
tjlyons <tjlyons@uncg.edu>
2025-09-04 10:40:42 -0400

    assessment01 attempt1

--- code/assessment01.R
+++ code/assessment01.R
@@ -5,28 +5,42 @@
 
 # 1: Create a vector with three elements. Assign it to `v_three`.
 
+v_three <- c("A","B","C")
+
 # 2: Create a vector containing 20 "a", 30 "b", and 50 "c" (total length = 100).  
 # Assign it to `v_abc100`.
+
+v_abc100 <- c(rep("a", 20), rep("b", 30), rep("c", 50))
 
 # 3: The script below creates a vector `v_x` with 100 random numbers from a normal distribution.  
 # Select only the positive numbers (> 0) from `v_x`, calculate their mean, and assign it to `mu_x_plus`.
 set.seed(100)
 v_x <- rnorm(100)
 
+mu_x_plus <- mean(v_x > 0)
+
 # 4: Create a numeric matrix with the numbers 1 through 9 arranged in 3 rows × 3 columns.  
 # Assign it to `m_num`.
 
+m_num <- matrix(data = 1:9, nrow = 3, ncol = 3)
+
 # 5: Create a base R data frame (`data.frame()` function) using `v_x` and `v_abc100`.  
 # Name the columns `"x"` for `v_x` and `"group"` for `v_abc`, and assign it to `df_sample`.
+
+data_frame()
 
 
 # tidyverse ---------------------------------------------------------------
 
 # 6: Load the `tidyverse` package.
 
+library(tidyverse)
+
 # 7: The `mtcars` dataset is a built-in base R data frame.  
 # Convert it to a tibble using `as_tibble()` and assign it to `df_mtcars`.  
 # Use `?as_tibble()` to read the documentation before doing so.
+
+df_mtcars <- as_tibble(mtcars)
 
 # 8: `mtcars` has the following columns:
 #
@@ -45,31 +59,68 @@ v_x <- rnorm(100)
 # Display the column names of `df_mtcars` using `colnames()`.  
 # Do NOT assign the result to a new object.
 
+colnames(df_mtcars)
+
 # 9: Extract the row names of the `mtcars` dataset using `rownames()`.  
 # Assign the result to `v_make`.
 
+v_make <- rownames(df_mtcars)
+
 # 10: Add `v_make` as a new column to `df_mtcars` and name the column `"make"`.
+
+df_mtcars %>% 
+  mutate(make = v_make)
 
 # 11: Filter `df_mtcars` to include only rows where:  
 # - `mpg` is less than 20 AND  
 # - `disp` is greater than 200  
 # Assign the result to `df_subset`.
 
+df_subset <- df_mtcars %>% 
+  filter(mpg < 20 & disp > 200)
+
 # 12: Count how many car makes meet the above conditions (Q11).
 # Apply `nrow()` to `df_subset`.
 
+nrow(df_subset)
+
 # 13: Repeat Q11 and Q12 in a single pipeline (with %>%), and assign the result to `n_make`.
+
+n_make <- df_mtcars %>% 
+  filter(mpg < 20 & disp > 200) %>% 
+  nrow()
 
 # 14: Convert the `cyl` column from numeric to factor using `factor()`.  
 # Add it to `df_mtcars` as a new column named `f_cyl` using `mutate()` function.
 
+df_mtcars2 <- df_mtcars %>%
+  mutate(f_cyl = as.factor(cyl))
+  
+
 # 15: Draw a box plot showing car weight (`wt`) for each number of cylinders (`f_cyl`).
 
-# 16: Calculate the average car weight (`wt`) separately for each number of cylinders (`cyl`).
+df_mtcars2 %>% 
+  ggplot(mapping = aes(x = wt,
+             y = f_cyl)) +
+  geom_boxplot()
+
+# # # 16: Calculate the average car weight (`wt`) separately for each number of cylinders (`cyl`).
+
+df_mtcars %>% 
+  group_by(cyl) %>% 
+  summarize(average = mean(wt))
 
 # 17: Identify the heaviest car make (`wt`) among cars with 6 cylinders (`cyl`).
 
+df_mtcars %>% 
+  group_by(cyl = 6) %>% 
+  summarize(max(wt))
+
 # 18: Create a histogram showing the distribution of 1/4 mile time (`qsec`).
+
+df_mtcars %>% 
+  ggplot(aes(x = qsec)) +
+           geom_histogram()
 
 # 19: The following script creates two tibbles:  
 # `df_length` (body length) and `df_weight` (body weight),  
@@ -82,15 +133,25 @@ v_w <- rnorm(n = length(v_l),
              mean = 0.1 * v_l^1.5,
              sd = 10)
 
+v_sp <- sample(c("bhc", "rbs", "gsf"),
+       size = length(v_l),
+       replace = TRUE)
+
 df_length <- tibble(length = v_l,
-                    sp_code = sample(c("bhc", "rbs", "gsf"),
-                                     size = length(v_l),
-                                     replace = TRUE))
+                    sp_code = v_sp)
 
 df_weight <- tibble(weight = v_w,
-                    sp_code = sample(c("bhc", "rbs", "gsf"),
-                                     size = length(v_l),
-                                     replace = TRUE))
+                    sp_code = v_sp)
+
+df_fish <- left_join(x = df_length,
+          y = df_weight,
+          by = "sp_code")
 
 # 20: Draw a scatter plot (point plot) of `length` vs. `weight` from `df_fish`,  
 # coloring the points by species code (`sp_code`).
+
+df_fish %>% 
+  ggplot(aes(x = length,
+             y = weight,
+             color = sp_code)) +
+  geom_point()
